f61b0813124b528e004019076923b9da1d66c78b
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-04-14 11:36:12 +0200

    chore: update rna config

--- rna.config.js
+++ rna.config.js
@@ -6,16 +6,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
     entrypoints: [],
     clean: true,
-    format: 'esm',
     sourcemap: !isProduction,
     entryNames: isProduction ? '[name]-[hash]' : '[name]',
     chunkNames: isProduction ? '[name]-[hash]' : '[name]',
     assetNames: isProduction ? '[name]-[hash]' : '[name]',
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxModule: '@chialab/dna',
     minify: isProduction,
-    bundle: true,
+    bundle: isProduction,
 };
 
 export default config;
