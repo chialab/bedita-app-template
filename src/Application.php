091147d8f703e3ed2c3f20f29c0448d516d66a99
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-12-21 08:52:50 +0100

    fix case for `BEdita/API` plugin which caused CSRF middleware to be loaded for REST APIs

--- src/Application.php
+++ src/Application.php
@@ -111,7 +111,7 @@ class Application extends BaseApplication
             // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
             ->add(new BodyParserMiddleware());
 
-        if (Configure::read('FrontendPlugin') !== 'BEdita/Api') {
+        if (Configure::read('FrontendPlugin') !== 'BEdita/API') {
             $middlewareQueue
                 // Cross Site Request Forgery (CSRF) Protection Middleware
                 // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
